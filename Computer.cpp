e185eb246094aaa0c9391f815ad26834b0955a4d
HarryCoren <hcoren@LAPTOP-CS0DSTOA.localdomain>
2024-02-29 12:01:11 +1030

    gdgdg?

---
+++ Computer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Computer.h"
+
+
+std::string Computer::getName(){
+    return "Computer";
+} 
+
+char Human::makeMove(){
+    char move = 'R';
+    return move;
+}
---
+++ Referee.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Player.h"
+
+#ifndef REFEREE_H
+#define REFEREE_H
+
+#include <iostream>
+
+class Referee{
+    public:
+        Referee();
+        Player * refGame(Player * player1, Player * player2);
+};
+
+#endif
+
