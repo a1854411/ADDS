51ab49045d8d90c59d81e54269175911c400ed19
a1854411 <126529057+a1854411@users.noreply.github.com>
2024-03-21 02:06:10 +0000

    f

--- r1.cpp
+++ r1.cpp
@@ -2,9 +2,14 @@
 #include <string>
 #include <cmath>
 
-int sum_of_powers(int n, int result){
+int sum_of_powers(int n){
     if (n==1){
         return 1;
     }
-    return sum_of_powers(n-1, result + pow(n,n));
+    //return sum_of_powers(n-1, acc + pow(n,n));
+    return sum_of_powers(n - 1) + pow(n,n);
 }
+
+//int main(){
+    //std::cout << sum_of_powers(5) << std::endl;
+//}
