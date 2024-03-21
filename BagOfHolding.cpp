9a7a497a7da2b5c24ffe8e83489fd6db97c0b41f
a1854411 <126529057+a1854411@users.noreply.github.com>
2024-03-21 03:17:23 +0000

    er

--- BagOfHolding.cpp
+++ BagOfHolding.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <string>
 #include "BagOfHolding.h"
+
+
+void BagOfHolding::addItem(std::string item){
+    bag.push_back(item);
+}
+
+std::string BagOfHolding::removeItem(){
+    srand(time(nullptr));
+    int spot = rand() % bag.size();
+    std::string item = bag.at(spot);
+    bag.erase(bag.begin() + spot);
+    return item;
+}
+
+std::string BagOfHolding::removeItem(std::string itemRemoved){
+    for (int i=0; i < bag.size(); i++){
+        if (bag[i] == itemRemoved){
+            std::string item = bag.at(i);
+            bag.erase(bag.begin() + i);
+            return item; 
+        }
+    }
+    return nullptr;
+}
--- BagOfHolding.h
+++ BagOfHolding.h
@@ -4,35 +4,14 @@
 #ifndef BAGOFHOLDING_H
 #define BAGOFHOLDING_H
 
-template<class B>
 
 class BagOfHolding {
     private:
-        std::vector<B> bag; 
+        std::vector<std::string> bag; 
     public:
-        void addItem(B item){
-            bag.push_back(item);
-        }
-
-        B removeItem(){
-            srand(time(nullptr));
-            int spot = rand() % bag.size();
-            B item = bag.at(spot);
-            bag.erase(bag.begin() + spot);
-            return item;
-        }
-
-        B removeItem(B itemRemoved){
-            for (int i=0; i < bag.size(); i++){
-                if (bag[i] == itemRemoved){
-                    B item = bag.at(i);
-                    bag.erase(bag.begin() + i);
-                    return item; 
-                }
-            }
-            return nullptr;
-
-        }
+        void addItem(std::string item);
+        std::string removeItem(); 
+        std::string removeItem(std::string item); 
 };
 
 #endif
