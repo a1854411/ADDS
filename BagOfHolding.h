31f8ed584b64f560c600abc01bd2143447a92343
a1854411 <126529057+a1854411@users.noreply.github.com>
2024-03-21 03:16:17 +0000

    er

--- BagOfHolding.h
+++ BagOfHolding.h
@@ -4,15 +4,35 @@
 #ifndef BAGOFHOLDING_H
 #define BAGOFHOLDING_H
 
-
+template<class B>
 
 class BagOfHolding {
     private:
-        std::vector<std::string> bag; 
+        std::vector<B> bag; 
     public:
-        void addItem(std::string item);
-        std::string removeItem(); 
-        std::string removeItem(std::string item); 
+        void addItem(B item){
+            bag.push_back(item);
+        }
+
+        B removeItem(){
+            srand(time(nullptr));
+            int spot = rand() % bag.size();
+            B item = bag.at(spot);
+            bag.erase(bag.begin() + spot);
+            return item;
+        }
+
+        B removeItem(B itemRemoved){
+            for (int i=0; i < bag.size(); i++){
+                if (bag[i] == itemRemoved){
+                    B item = bag.at(i);
+                    bag.erase(bag.begin() + i);
+                    return item; 
+                }
+            }
+            return nullptr;
+
+        }
 };
 
 #endif
