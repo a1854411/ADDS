931bba9f435e6c5295333724806a4b903a888141
a1854411 <a1854411@adelaide.edu.au>
2024-03-28 12:37:31 +1030

    wefw

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -4,12 +4,10 @@ int Truckloads::numTrucks(int numCrates, int loadSize){
     if (numCrates <= loadSize){
         return 1;
     }
-    else{
         if(numCrates % 2 == 0){
             return 2*numTrucks(numCrates/2, loadSize);
         }
         else{
             return numTrucks((numCrates/2)-0.5, loadSize) + numTrucks((numCrates/2)+0.5, loadSize);
         }
-    }
-}   
+}
