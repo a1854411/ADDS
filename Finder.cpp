6daf630fa66b34c9b8774a7d78b7266e13fce97b
a1854411 <a1854411@adelaide.edu.au>
2024-03-30 22:08:21 +1030

    rr

--- Finder.cpp
+++ Finder.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2){
     vector<int> result; //initialise vector to be filled with indexes of s2 prefixes in s1
     for(size_t i = 1; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i)); //where found is the vector index position in s1
+        size_t found = s1.find(s2.substr(0, i), 0); //where found is the vector index position in s1
 
         if (found != string::npos) { //if position of prefix is not the same as the already found postition 
             result.push_back(found); //add index position to result vector
