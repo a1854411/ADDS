13f98a130a3fc840bef0d9e932dca13761b2a14d
a1854411 <a1854411@adelaide.edu.au>
2024-04-28 23:04:32 +0930

    wefwer

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -6,81 +6,143 @@ LinkedList::LinkedList(){
     head = nullptr;
 }
 
-LinkedList::LinkedList(int* array, int len){
-    Node* current_node= new Node(array[0], nullptr);
-    head = current_node;
+// LinkedList::LinkedList(int* array, int len){
+//     Node* current_node= new Node(array[0], nullptr);
+//     head = current_node;
 
-    for(int i=1; i<len; i++){
-        Node* new_node= new Node(array[i], nullptr);
+//     for(int i=1; i<len; i++){
+//         Node* new_node= new Node(array[i], nullptr);
+//         current_node->link = new_node;
+//         current_node = new_node;
+//     }
+// }
+
+LinkedList::LinkedList(int* array, int len) {
+    head = nullptr;
+    for (int i=0; i < len; i++) {
+        insertPosition(i+1, array[i]);}
+}
+
+LinkedList::~LinkedList() {
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->link;
+        delete temp;}
+}
+
+
+// void LinkedList::printList(){
+//     Node* current_node = head;
+
+//     std::cout << "[";
+//     while(current_node != nullptr){
+//         std::cout << current_node->data; 
+//         current_node = current_node->link;
+//     }
+//     std::cout << "]";
+// }
+
+void LinkedList::printList() {
+    std::cout << '[';
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->data;
+        if (current->link != nullptr) cout << ' ';
+        current = current->link;}
+    cout << ']' << endl;
+}
+
+
+// void LinkedList::insertPosition(int pos, int newNum){
+//     if(pos <= 1){
+//         head = new Node(newNum, head);
+//         return;
+//     }
+
+//     Node* current_node = head;
+
+//     for(int i=2; i<(pos-1); i++){
+//         if(current_node->link == nullptr){
+//             Node* new_node = new Node(newNum, nullptr);
+//             current_node->link = new_node;
+//             return;
+//         }
+//         Node* new_node = new Node(newNum, current_node->link);
+//         current_node = current_node->link;
+//         current_node->link = new_node;
+
+//     }
+// }
+
+void LinkedList::insertPosition(int pos, int newNum) {
+    Node* new_node = new Node(newNum, nullptr);
+    if(pos <= 1 || head == nullptr){
+        new_node->link = head;
+        head = new_node;}
+    else{
+        Node* current_node = head;
+        for (int i=1; i < pos - 1 && current_node->link != nullptr; i++) {
+            current_node = current_node->link;}
+        new_node->link = current_node->link;
         current_node->link = new_node;
-        current_node = new_node;
-
     }
 }
 
+// bool LinkedList::deletePosition(int pos){
+//     if (pos <= 0 || head == nullptr){ // Check for invalid position or empty list
+//         return false;
+//     }
 
-void LinkedList::printList(){
-    Node* current_node = head;
+//     Node* current_node = head;
+//     Node* previous_node = nullptr;
 
-    std::cout << "[";
-    while(current_node != nullptr){
-        std::cout << current_node->data; 
-        current_node = current_node->link;
-    }
-    std::cout << "]";
-}
+//     //delete at start
+//     if(pos == 1){
+//         head = current_node->link; //set head to next node
+//         delete current_node;
+//         return true;
+//     }
 
+//     //traverse the list
+//     for(int i=2; current_node!=nullptr && i<(pos-1); i++){
+//         previous_node = current_node;
+//         current_node = current_node->link;
+//     }
+//         // If position exceeds the size of the list
+//         if (current_node == nullptr){
+//             return false;
+//         }
 
-void LinkedList::insertPosition(int pos, int newNum){
-    if(pos <= 1){
-        head = new Node(newNum, head);
-        return;
-    }
+//         previous_node->link = current_node->link;
+//         delete current_node;
 
-    Node* current_node = head;
+//         return true;
+// }
 
-    for(int i=2; i<(pos-1); i++){
-        if(current_node->link == nullptr){
-            Node* new_node = new Node(newNum, nullptr);
-            current_node->link = new_node;
-            return;
-        }
-        Node* new_node = new Node(newNum, current_node->link);
-        current_node = current_node->link;
-        current_node->link = new_node;
-
-    }
-}
-
-bool LinkedList::deletePosition(int pos){
-    if (pos <= 0 || head == nullptr){ // Check for invalid position or empty list
+bool LinkedList::deletePosition(int pos) {
+    if (pos<1 || head == nullptr) {
         return false;
     }
 
-    Node* current_node = head;
-    Node* previous_node = nullptr;
-
-    //delete at start
-    if(pos == 1){
-        head = current_node->link; //set head to next node
-        delete current_node;
-        return true;
+    if (pos == 1) {
+        Node* temp = head;
+        head = head->link;
+        delete temp;
+    }else{
+        Node* current_node = head;
+        for (int i = 1; i < pos - 1 && current_node != nullptr; i++) {
+            current_node = current_node->link;
     }
-
-    //traverse the list
-    for(int i=2; current_node!=nullptr && i<(pos-1); i++){
-        previous_node = current_node;
-        current_node = current_node->link;
+        
+    if (current_node == nullptr || current_node->link == nullptr) {
+        return false;
     }
-        // If position exceeds the size of the list
-        if (current_node == nullptr){
-            return false;
-        }
-
-        previous_node->link = current_node->link;
-        delete current_node;
-
-        return true;
+        
+    Node* temp = current_node->link;
+    current_node->link = temp->link;
+    delete temp;
+    }
+    return true;
 }
 
 
@@ -101,7 +163,7 @@ int LinkedList::get(int pos){
 
 int LinkedList::search(int target){
     int index = 1;
-    for (Node* current_node = this->head; current_node != nullptr; current_node = current_node->link) {
+    for (Node* current_node = head; current_node != nullptr; current_node = current_node->link) {
         if (current_node->data == target) {
             return index;}
         index++;}
