a067b60e4ca7f3c63d11e2f1c1d75a65e97eea40
a1854411 <126529057+a1854411@users.noreply.github.com>
2024-05-02 03:47:49 +0000

    wefwe

---
+++ LinkedList.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "LinkedList.h"
+
+LinkedList::LinkedList() {
+	head = nullptr;
+}
+
+LinkedList::~LinkedList() {
+	while (head != nullptr) {
+		deleteFront();
+	}
+}
+
+void LinkedList::insertFront(int item) {
+	Node* newNode = new Node(item, head);
+	head = newNode;
+}
+
+void LinkedList::deleteFront() {
+	if(head != nullptr) {
+		Node* oldHead = head;
+		head = head->link;
+		delete oldHead;
+	}
+}
+
+void LinkedList::printList() {
+	Node* currNode = head;
+	while(currNode != nullptr) {
+		std::cout << currNode->data << " ";
+		currNode = currNode->link;
+	}
+}
+
+/////////// your code goes here... DO NOT change the function signatures ///////////
+
+bool LinkedList::swap(int pos1, int pos2) { 
+// swaps the data at positions pos1 and pos2 then returns true.
+// If a or b are out-of-bounds, return false without swapping any elements.
+    int aNum, bNum;
+    Node* a = new Node(aNum, nullptr);
+    Node* b = new Node(bNum, nullptr);
+
+    // swap(a,b);
+
+    if (pos1<1 || pos2<1 || head == nullptr){
+        return false;
+    }
+
+    // if (currNode == nullptr){
+    //     return false;
+    // }
+
+
+
+}
+
+bool LinkedList::find_and_delete(int target){
+// searches the list for a node containing target, which then deletes it and returns true. [3 marks]
+// If target appears multiple times in the list, delete the first occurrence.
+// If target does not exist in the list return false.
+
+    int index = 1;
+    for (Node* currNode = head; currNode != nullptr; currNode = currNode->link){
+        if(currNode->data == target){
+            
+            Node* temp = currNode->link;
+            currNode->link = temp->link;
+            delete temp;
+
+            return true;
+        }
+    }
+    return false;
+}
---
+++ smartness.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>      /* printf */
+#include <math.h>       /* log */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+//   double param, result;
+//   param = 550;
+//   result = log (param*param);
+//   std::cout << result << std::endl;
+//   return 0;
+
+  //=12.6198
+  //2
+
+// double param, result;
+//   param = 550;
+//   result = param*param + param;
+//   std::cout << result << std::endl;
+//   return 0;
+
+  //=303050
+  //6
+
+
+//     double param, result;
+//   param = 5;
+//   result = pow(log(1),2*param);
+//   std::cout << result << std::endl;
+//   return 0;
+
+//   //=0
+//1
+
+//   double param, result;
+//   param = 550;
+//   result = pow(1.5,param);
+//   printf ("log(%f) = %f\n", param, result );
+//   return 0;
+
+  //big number
+  //7
+
+// double param, result;
+//   param = 550;
+//   result = param * log (pow(param,3));
+//   printf ("log(%f ^2) = %f\n", param, result );
+//   return 0;
+
+  //=10411
+  //5
+
+//   double param, result;
+//   param = 550;
+//   result = pow(param,0.5)+1;
+//   printf ("log(%f ^2) = %f\n", param, result );
+//   return 0;
+
+  //=24.45
+  //3
+
+
+  //n! = 1271
+//4
+
+}
+
