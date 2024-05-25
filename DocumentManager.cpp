2f4f6e93d97cfd2c4e6b5bdd8ad31ffe8c233f12
a1854411 <a1854411@adelaide.edu.au>
2024-05-25 13:27:06 +0930

    wegg

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,10 +1,1 @@
 #include "DocumentManager.h"
-
-void DocumentManager::addDocument(std::string name, int id, int license_limit){
-            Document doc(name, id, license_limit);
-            Documents.emplace(doc);
-}
-void DocumentManager::addPatron(int patronID){}
-int DocumentManager::search(std::string name){} // returns docid if name is in the document collection or 0 if the name is not in the collection
-bool DocumentManager::borrowDocument(int docid, int patronID){}  // returns true if document is borrowed, false if it can not be borrowed (invalid patronid or the number of copies current borrowed has reached the license limit)
-void DocumentManager::returnDocument(int docid, int patronID){}
