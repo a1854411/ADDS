73200537af871bcd5b81420a82dec7f291063698
a1854411 <a1854411@adelaide.edu.au>
2024-06-04 15:47:44 +0930

    wefwe" git commit -m wefwe git commit -m 'wefwe

--- CompilerParser.cpp
+++ CompilerParser.cpp
@@ -37,6 +37,126 @@ ParseTree* CompilerParser::compileProgram() {
     return newTree;
 }
 
+/**
+ * Generates a parse tree for a single class
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileClass() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a static variable declaration or field declaration
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileClassVarDec() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a method, function, or constructor
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileSubroutine() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a subroutine's parameters
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileParameterList() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a subroutine's body
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileSubroutineBody() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a subroutine variable declaration
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileVarDec() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a series of statements
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileStatements() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a let statement
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileLet() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for an if statement
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileIf() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a while statement
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileWhile() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a do statement
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileDo() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for a return statement
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileReturn() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for an expression
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileExpression() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for an expression term
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileTerm() {
+    return NULL;
+}
+
+/**
+ * Generates a parse tree for an expression list
+ * @return a ParseTree
+ */
+ParseTree* CompilerParser::compileExpressionList() {
+    return NULL;
+}
+
 void CompilerParser::next() {
     if (it != tokens.end()) {
         ++it;
