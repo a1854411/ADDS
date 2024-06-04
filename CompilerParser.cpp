1f74e7b3b7fb0f594f20782a2191ddd123b51702
a1854411 <a1854411@adelaide.edu.au>
2024-06-04 15:38:06 +0930

    ewfwe

--- CompilerParser.cpp
+++ CompilerParser.cpp
@@ -1,228 +1,74 @@
 #include "CompilerParser.h"
 
-/**
- * Constructor for the CompilerParser
- * @param tokens A linked list of tokens to be parsed
- */
-CompilerParser::CompilerParser(std::list<Token*> tokens) {
-    this->tokens = tokens;
-    it = tokens.begin();
+CompilerParser::CompilerParser(std::list<Token*> tokens) : tokens(tokens) {
+    it = this->tokens.begin();
 }
 
-/**
- * Generates a parse tree for a single program
- * @return a ParseTree
- */
 ParseTree* CompilerParser::compileProgram() {
-    ParseTree *newTree = new ParseTree("class", "");
-    //get class
-    if(current() == mustBe("keyword","class")){
+    ParseTree* newTree = new ParseTree("class", "");
+    if (mustBe("keyword", "class")) {
         newTree->addChild(new ParseTree(current()->getType(), current()->getValue()));
         next();
-    }
-    else{
-        throw ParseException();
-    }
-    
-    //get indentifier
-    if(current() == mustBe("identifier", "MyClass")){
-        newTree->addChild(new ParseTree(current()->getType(), current()->getValue()));
-        next();
-    }
-    else{
+    } else {
         throw ParseException();
     }
 
-    //get symbol
-    if(current() == mustBe("symbol", "{")){
+    if (mustBe("identifier", "MyClass")) {
         newTree->addChild(new ParseTree(current()->getType(), current()->getValue()));
         next();
-    }
-    else{
+    } else {
         throw ParseException();
     }
 
-    //get symbol
-    if(current() == mustBe("symbol", "}")){
+    if (mustBe("symbol", "{")) {
         newTree->addChild(new ParseTree(current()->getType(), current()->getValue()));
         next();
+    } else {
+        throw ParseException();
     }
-    else{
+
+    if (mustBe("symbol", "}")) {
+        newTree->addChild(new ParseTree(current()->getType(), current()->getValue()));
+        next();
+    } else {
         throw ParseException();
     }
 
     return newTree;
 }
 
-
-/**
- * Generates a parse tree for a single class
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileClass() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a static variable declaration or field declaration
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileClassVarDec() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a method, function, or constructor
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileSubroutine() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a subroutine's parameters
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileParameterList() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a subroutine's body
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileSubroutineBody() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a subroutine variable declaration
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileVarDec() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a series of statements
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileStatements() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a let statement
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileLet() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for an if statement
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileIf() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a while statement
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileWhile() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a do statement
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileDo() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for a return statement
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileReturn() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for an expression
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileExpression() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for an expression term
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileTerm() {
-    return NULL;
-}
-
-/**
- * Generates a parse tree for an expression list
- * @return a ParseTree
- */
-ParseTree* CompilerParser::compileExpressionList() {
-    return NULL;
-}
-
-/**
- * Advance to the next token
- */
-void CompilerParser::next(){
-    if(it != tokens.end()){
+void CompilerParser::next() {
+    if (it != tokens.end()) {
         ++it;
     }
-    return;
 }
 
-/**
- * Return the current token
- * @return the Token
- */
-Token* CompilerParser::current(){
-    return *it;
+Token* CompilerParser::current() {
+    if (it != tokens.end()) {
+        return *it;
+    }
+    return nullptr;
 }
 
-/**
- * Check if the current token matches the expected type and value.
- * @return true if a match, false otherwise
- */
-bool CompilerParser::have(std::string expectedType, std::string expectedValue){
-    if(current()->getType() == expectedType && current()->getValue() == expectedValue){
-        return true;
+bool CompilerParser::have(std::string expectedType, std::string expectedValue) {
+    Token* token = current();
+    if (token) {
+        return token->getType() == expectedType && token->getValue() == expectedValue;
     }
     return false;
 }
 
-/**
- * Check if the current token matches the expected type and value.
- * If so, advance to the next token, returning the current token, otherwise throw a ParseException.
- * @return the current token before advancing
- */
-Token* CompilerParser::mustBe(std::string expectedType, std::string expectedValue){
-    if(have(expectedType, expectedValue)) {
-        Token* current_token = current();
+Token* CompilerParser::mustBe(std::string expectedType, std::string expectedValue) {
+    if (have(expectedType, expectedValue)) {
+        Token* token = current();
         next();
-        return current_token;
+        return token;
     } else {
         throw ParseException();
     }
 }
 
-/**
- * Definition of a ParseException
- * You can use this ParseException with `throw ParseException();`
- */
-const char* ParseException::what() {
-    return "An Exception occurred while parsing!";
+const char* ParseException::what() const noexcept {
+    return "An exception occurred while parsing!";
 }
+
--- CompilerParser.h
+++ CompilerParser.h
@@ -3,46 +3,42 @@
 
 #include <list>
 #include <exception>
-
 #include "ParseTree.h"
 #include "Token.h"
 
 class CompilerParser {
-    private:
-        std::list<Token*> tokens;
-        std::list<Token*>::iterator it;
-    public:
-        CompilerParser(std::list<Token*> tokens);
-        
-        ParseTree* compileProgram();
-        ParseTree* compileClass();
-        ParseTree* compileClassVarDec();
-        ParseTree* compileSubroutine();
-        ParseTree* compileParameterList();
-        ParseTree* compileSubroutineBody();
-        ParseTree* compileVarDec();
+private:
+    std::list<Token*> tokens;
+    std::list<Token*>::iterator it;
 
-        ParseTree* compileStatements();
-        ParseTree* compileLet();
-        ParseTree* compileIf();
-        ParseTree* compileWhile();
-        ParseTree* compileDo();
-        ParseTree* compileReturn();
-
-        ParseTree* compileExpression();
-        ParseTree* compileTerm();
-        ParseTree* compileExpressionList();
-        
-        void next();
-        Token* current();
-        bool have(std::string expectedType, std::string expectedValue);
-        Token* mustBe(std::string expectedType, std::string expectedValue);
+public:
+    CompilerParser(std::list<Token*> tokens);
+    ParseTree* compileProgram();
+    ParseTree* compileClass();
+    ParseTree* compileClassVarDec();
+    ParseTree* compileSubroutine();
+    ParseTree* compileParameterList();
+    ParseTree* compileSubroutineBody();
+    ParseTree* compileVarDec();
+    ParseTree* compileStatements();
+    ParseTree* compileLet();
+    ParseTree* compileIf();
+    ParseTree* compileWhile();
+    ParseTree* compileDo();
+    ParseTree* compileReturn();
+    ParseTree* compileExpression();
+    ParseTree* compileTerm();
+    ParseTree* compileExpressionList();
+    void next();
+    Token* current();
+    bool have(std::string expectedType, std::string expectedValue);
+    Token* mustBe(std::string expectedType, std::string expectedValue);
 };
 
 class ParseException : public std::exception {
-    public:
-        const char* what();
+public:
+    const char* what() const noexcept override;
 };
 
-#endif /*COMPILERPARSER_H*/
+#endif
 
