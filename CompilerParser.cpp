af3f69bfd3302953430b9050089b0dea3d16acf9
a1854411 <a1854411@adelaide.edu.au>
2024-06-07 15:39:49 +0930

    ewfwe

--- CompilerParser.cpp
+++ CompilerParser.cpp
@@ -233,40 +233,6 @@ ParseTree* CompilerParser::compileExpressionList() {
     return newTree;
 }
 
-void CompilerParser::next() {
-    if (it != tokens.end()) {
-        ++it;
-    }
-}
-
-Token* CompilerParser::current() {
-    if (it != tokens.end()) {
-        return *it;
-    }
-    return nullptr;
-}
-
-bool CompilerParser::have(std::string expectedType, std::string expectedValue) {
-    Token* token = current();
-    if (token) {
-        return token->getType() == expectedType && (expectedValue.empty() || token->getValue() == expectedValue);
-    }
-    return false;
-}
-
-Token* CompilerParser::mustBe(std::string expectedType, std::string expectedValue) {
-    if (have(expectedType, expectedValue)) {
-        Token* token = current();
-        next();
-        return token;
-    } else {
-        throw ParseException();
-    }
-}
-
-const char* ParseException::what() const noexcept {
-    return "An exception occurred while parsing!";
-}
 
 void CompilerParser::next() {
     if (it != tokens.end()) {
